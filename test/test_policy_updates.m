58429f57bd3b8902164f3616dd2b01070dbf1839
Ricardo <riddoddo@gmail.com>
2018-03-14 21:06:35 +0000

    Policy update fix 2

--- test/test_policy_updates.m
+++ test/test_policy_updates.m
@@ -1,11 +1,22 @@
 init_param;
 init_arbitrary_pol;
 og = hipol.muW;
-load('d2RolloutData.mat')
-train_forward_model
-predict_reward
-update_policy
-differences = (og - hipol.muW)
-sum(abs(og-hipol.muW))
+load('d4RolloutData.mat')
+eps = 3;
+kn = 10;
+policies = zeros(kn+1, length(hipol.muW));
+policies(1, :) = hipol.muW';
+for kiii = 1:kn
+    train_forward_model
+    predict_reward
+    update_policy
+    differences = (hipol.muW - og);
+    sum(abs(og-hipol.muW))
+    policies(kiii+1, :) = hipol.muW';
+    %hipol.muW
+    %pause;
+end
 
-% eps 1 - 157
+%plot(policies')
+%num2cell('123456')
+%legend(ans)
